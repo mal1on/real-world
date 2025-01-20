24f8561b02c403194cc3c6de2b345eb6b3914df8
mal1on <lyubo.malinov@gmail.com>
2025-01-20 23:28:28 +0200

    First working version

--- Extract the first sentence of every text file/main.py
+++ Extract the first sentence of every text file/main.py
@@ -7,13 +7,35 @@ import re
 import os
 
 
+directory = 'tmp'
+
+
 def get_first_sentence(text):
     '''Function to get the first sentence'''
 
-    pattern = r'[A-Z][^\.!\?]*[\.\!\?]'
-    sentence = re.match(pattern, text)
+    pattern = r'[A-Z][^.!?\n]+[\.\!\?]'
+    match = re.search(pattern, text)
 
-    return sentence.group(0)
+    return match.group() if match else None
 
 
-get_first_sentence()
+def main():
+    '''Main function'''
+
+    if os.path.exists(directory):
+        files = os.listdir(directory)
+        for file in files:
+            path = os.path.join(directory, file)
+            try:
+                with open(path, 'r', encoding='utf-8') as f:
+                    sentence = get_first_sentence(f.read())
+                    if sentence:
+                        print(sentence)
+            except Exception as e:
+                print(f'Error opening {file}: {e}')
+    else:
+        print(f'{directory} does not exist')
+
+
+if __name__ == '__main__':
+    main()
