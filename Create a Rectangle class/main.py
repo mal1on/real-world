6ed675a79851a0dac62a44ea447f4c83828c5a76
mal1on <lyubo.malinov@gmail.com>
2026-01-05 15:37:53 +0200

    First working version

---
+++ Create a Rectangle class/main.py
@@ -0,0 +1,15 @@
+class Rectangle:
+    """Python class that represents a rectangle geometrical figure."""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def is_square(self):
+        return self.width == self.height
