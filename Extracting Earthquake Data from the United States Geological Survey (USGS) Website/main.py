a5abd21c2913565a17edc0af55d60f3ed4f2710f
mal1on <lyubo.malinov@gmail.com>
2026-01-27 20:41:39 +0200

    First working version

--- Extracting Earthquake Data from the United States Geological Survey (USGS) Website/main.py
+++ Extracting Earthquake Data from the United States Geological Survey (USGS) Website/main.py
@@ -1,9 +1,11 @@
 """
-Аccess the United States Geological Survey free API and download
+Access the United States Geological Survey free API and download
 real-time earthquake events.
 """
 
+import os
 import requests
+import pandas as pd
 
 
 URL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
@@ -16,8 +18,32 @@ def data_getter():
     if response.status_code != 200:
         raise Exception(f'API error: {response.status_code}')
 
-    data = response.json()
+    earthquakes = response.json()['features']
 
-    return data
+    data_list = []
 
-print(data_getter())
+    for earthquake in earthquakes:
+        mag = earthquake['properties']['mag']
+        place = earthquake['properties']['place']
+        lon, lat, _ = earthquake['geometry']['coordinates']
+        data_list.append({
+            'Magnitude': mag,
+            'Location': place,
+            'Latitude': lat,
+            'Longitude': lon
+             })
+
+    return data_list
+
+
+def csv_maker(data):
+    """Create csv file from the available data"""
+
+    os.makedirs('tmp', exist_ok=True)
+    df = pd.DataFrame(data)
+    df.to_csv('tmp/output.csv', index=False)
+
+
+if __name__ == '__main__':
+    data = data_getter()
+    csv_maker(data)
